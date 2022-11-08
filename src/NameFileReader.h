191d7880515585bd3d0b71be74da134d96764726
Yehonatan Katz <yehonatankatz@gmail.com>
2022-11-08 15:23:55 -0500

    edited namefilereader header

--- src/NameFileReader.h
+++ src/NameFileReader.h
@@ -2,8 +2,15 @@
 #define NAME_FILE_READER_H_
 
 #include "FileReader.h"
+#include <iostream>
+using namespace std;
+
+#define IDLEN 9
+#define NAMELEN 32
+
 
 class NameFileReader : public FileReader {
+    int VerifyLine(string line);
     public:
         using FileReader::FileReader;
         void read_file(StudentList s);
