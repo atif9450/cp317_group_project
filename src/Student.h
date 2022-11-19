6337b35f6b7eb6d7a8a41b1366c5c1150911840f
mlvfood <101908304+mlvfood@users.noreply.github.com>
2022-11-19 18:40:14 -0500

    Update Student.h

--- src/Student.h
+++ src/Student.h
@@ -6,12 +6,14 @@
 #include <fstream>
 
 class Student {
-    std::string name;
+    std::string student_name;
     int student_ID;
+    // string name;
     std::map<std::string, float> grades;
 
     public:
-        void create_student(int student_ID, std::string student_name);
+        // void create_student(int student_ID, std::string student_name);
+        Student create_student(int student_ID, std::string student_name);
         void write_final_grades(std::string file_name);
         void add_course(std::string course, float Test_1, float Test_2, float Test_3, float Test_4);
 };
