5f06678c237766f5df42b4b914c4aede3c67ef06
mlvfood <101908304+mlvfood@users.noreply.github.com>
2022-11-19 18:44:28 -0500

    Update StudentList.cpp

--- src/StudentList.cpp
+++ src/StudentList.cpp
@@ -1,5 +1,16 @@
 #include "StudentList.h"
 
-Student* StudentList::get_student(int student_ID) {return NULL;}
+Student* StudentList::get_student(int student_ID) {
+    Student obj;
+    for (int i = 0; i<list.size(); i++){
+        if(list[i].student_ID == student_ID){
+            obj = list[i];
+        }
+    }
+    return obj;
+    }
 
-void StudentList::add_student(int student_ID, std::string name) {}
+void StudentList::add_student(int student_ID, std::string name) {
+    Student obj = create_student(student_ID, name);
+    list.insert(obj);
+}
