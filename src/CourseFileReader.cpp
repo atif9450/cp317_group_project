e80e484b2ba4d9767cb9258c30c398e596c0e57b
Rehma Ashraf <111317223+ashr3960@users.noreply.github.com>
2022-11-23 08:36:59 -0500

    Update CourseFileReader.cpp

--- src/CourseFileReader.cpp
+++ src/CourseFileReader.cpp
@@ -1,5 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <string>
 #include "CourseFileReader.h"
 
-void CourseFileReader::read_file(StudentList s) {
-    
+using namespace std;
+
+void CourseFileReader:: read_file(StudentList s) {
+    string line;
+    stringstream ss;
+
+    try {
+        VerifyLine(line);
+    }
+    catch (invalid_argument & e) {
+        cerr << e.what() << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream ReadFile(file_name);
+    int id;
+    string name;
+    int test1;
+    int test2;
+    int test3;
+    int finaltest;
+    string temp;
+
+
+    while (getline(ReadFile, line)){
+        // add each student
+        stringstream ss(line);
+        id = stoi(getline(ss, id, ','));
+        getline(ss, name, ',');
+        getline(ss, temp, ',');
+        test1 = stoi(temp);
+        getline(ss, temp, ',');
+        test2 = stoi(temp);
+        getline(ss, temp, ',');
+        test3 = stoi(temp);
+        getline(ss, temp, ',');
+        finaltest = stoi(temp);
+
+        s.addStudent(id, name, test1, test2, test3, final);
+
+    }
+    return 0;
+
+    ReadFile.close();
+
 }
