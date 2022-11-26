59f691a6c85197dcb971b81fd9041a7f0f0cff2f
mlvfood <101908304+mlvfood@users.noreply.github.com>
2022-11-26 12:33:07 -0500

    Update Student.cpp

--- src/Student.cpp
+++ src/Student.cpp
@@ -1,17 +1,24 @@
 #include "Student.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 // void Student::create_student(int ID, std::string name) {
-Student::create_student(int ID, std::string name) {
+Student Student::create_student(int ID, std::string name) {
     Student student;
     student.student_ID = ID;
     student.student_name = name;
 
+    std:: cout << student.student_ID << "\n";
+    std:: cout << student.student_name;
+    
     return student;
 }
 
 void Student::write_final_grades(std::string file_name) {
-    ofstream File(file_name);
+    fstream File(file_name);
 
+    map<string,float>::iterator count;
     for (count = grades.begin(); count != grades.end(); ++count) {
         File << student_name << '\t'<< student_ID << '\t'<< count->first<< '\t' << count->second << '\n';
         // outdata << num[i] << endl;
@@ -21,8 +28,9 @@ void Student::write_final_grades(std::string file_name) {
 }
 
 void Student::add_course(std::string course, float Test_1, float Test_2, float Test_3, float Test_4) {
-    grade = (Test_1+Test_2+Test_3Test_4)/4;
-    float final_grade = floor(grade * 100.0) / 100.0;
+    float grade = (0.2*Test_1) + (0.2*Test_2)+(0.2*Test_3)+(0.4*Test_4);
+    float final_grade = (grade * 10.0) / 10.0;
 
     grades.insert(pair<string, float>(course, final_grade));
 }
+
